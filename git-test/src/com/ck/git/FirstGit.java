48da8161800a265969c17a36d1efe8cf1051af9d
yyx <1536309361@qq.com>
2020-06-26 16:42:58 +0800

    把工程提交到Git中

---
+++ git-test/src/com/ck/git/FirstGit.java
@@ -0,0 +1,12 @@
+package com.ck.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("heppl");
+        System.out.println("heppl");
+        System.out.println("hepp2");
+        System.out.println("hepp3");
+        System.out.println("hepp4");
+        System.out.println("hepp5");
+    }
+}
